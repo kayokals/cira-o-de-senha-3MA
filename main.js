c140b3bcc944ad6fa245d1c47894fb21130698aa
kayokals <141026718+kayokals@users.noreply.github.com>
2025-06-11 12:26:27 -0300

    11/06/25

---
+++ main.js
@@ -0,0 +1,19 @@
+const numeroSenha = document.querySelector('parametro-senha__texto');
+let tamanhoSenha = 12
+
+const botoes = document.querySelectorAll(".parametro-senha__botao")
+botoes[0].onclick = diminuirTamanho;
+botoes[1].onclick = aumentarTamanho;
+function diminuiTamanho(){
+    if(tamanhoSenha > 1) {
+        tamanhoSenha
+    }
+    numeroSenha.textContent = tamanhoSenha;
+};
+
+function aumentarTamanho(){
+    if(tamanhoSenha <20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+};
